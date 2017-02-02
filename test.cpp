f73d635c2ac504d2637691562b74f578833abf3f
Brad Richardson <brichardson@structint.com>
2017-02-02 13:13:13 -0800

    Write next test case

--- test.cpp
+++ test.cpp
@@ -12,4 +12,12 @@ TEST_CASE("Test the Bolwing Game", "[game]") {
         REQUIRE(g->score() == 0);
     }
 
+    SECTION("Test All Ones") {
+        Game *g = new Game();
+        for (int i=0; i<20; i++) {
+            g->roll(1);
+        }
+        REQUIRE(g->score() == 20);
+    }
+
 }
