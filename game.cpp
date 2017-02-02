633d9e443bf7b3ded9a09054a152f3eeaf30ab28
Brad Richardson <brichardson@structint.com>
2017-02-02 13:09:56 -0800

    Get the test to compile again

--- game.cpp
+++ game.cpp
@@ -4,3 +4,7 @@ void Game::roll(int pins) {
     int temp=pins;
     temp++;
 }
+
+int Game::score() {
+    return -1;
+}
--- game.h
+++ game.h
@@ -3,5 +3,6 @@
 class Game {
   public:
     void roll(int pins);
+    int score();
 };
 #endif
