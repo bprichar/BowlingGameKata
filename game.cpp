c4a8e49af01b0b212da094c07bc6da1de2c302d8
Brad Richardson <brichardson@structint.com>
2017-02-02 18:59:55 -0800

    Calculate score from each roll

--- game.cpp
+++ game.cpp
@@ -1,10 +1,20 @@
 #include "game.h"
 
+Game::Game() {
+    current_roll = 0;
+    my_score = 0;
+    rolls.resize(21);
+}
+
 void Game::roll(int pins) {
     my_score += pins;
     rolls[current_roll++] = pins;
 }
 
 int Game::score() {
+    int my_score = 0;
+    for (std::size_t i = 0; i < rolls.size(); i++) {
+        my_score += rolls[i];
+    }
     return my_score;
 }
--- game.h
+++ game.h
@@ -1,13 +1,17 @@
 #ifndef THE_GAME
 #define THE_GAME 1
+
+#include <vector>
+
 class Game {
   public:
+    Game();
     void roll(int pins);
     int score();
 
   private:
     int my_score;
-    int rolls[21];
+    std::vector<int> rolls;
     int current_roll;
 };
 #endif
